705cfbfaf8e83da26dc98af1b5065bca06af2164
Droid021 <vincivenv@gmail.com>
2020-09-11 12:17:41 +0300

    feat: Header component

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from "@material-ui/icons/Search"
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket"
 
 function Header() {
     return (
         <div className='header'>
             <img className='header__logo'
                 src='https://1079life-com.exactdn.com/wp-content/uploads/2018/12/amazon_PNG11.png'></img>
+
             {/*search bar*/}
             <div className='header__search'>
                 <input className='header__searchInput' type='text' />
-                <SearchIcon className="header__searchIcon"/>
+                <SearchIcon className="header__searchIcon" />
             </div>
 
             <div className="header__nav">
@@ -29,6 +31,10 @@ function Header() {
                     <span className="header__optionLineTwo">Prime</span>
                 </div>
 
+                <div className="header__optionBasket">
+                    <ShoppingBasketIcon />
+                    <span className="header__optionLineTwo header__basketCount">0</span>
+                </div>
             </div>
         </div>
     )
