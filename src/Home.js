84c6facd730f6be9ca57d8907c7c594ffef6fbb8
Droid021 <vincivenv@gmail.com>
2020-09-17 08:14:25 +0300

    chore: Update readme and some images

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div className='home'>
             <div className="home__container">
-                <img className='home__image' src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg" alt="Hero" />
+                <img className='home__image' src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/May/Hero/Fuji_TallHero_45M_v2_1x._CB432458380_.jpg" alt="Hero" />
             </div>
             {/* Product Id, title, prince, rating, image */}
             <div className="home__row">
@@ -24,10 +24,24 @@ function Home() {
                     rating={5}
                     image="https://images-na.ssl-images-amazon.com/images/I/51T-sMqSMiL._SX329_BO1,204,203,200_.jpg"
                 />
+                <Product
+                    id="3215"
+                    title="BENGOO G9000 Stereo Gaming Headset for PS4, PC, Xbox One Controller, Noise Cancelling Over Ear Headphones "
+                    price={9361}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/S/aplus-media/vc/e955b8b4-ae4f-4dc8-b629-dd387e5c69fc.__CR0,0,970,600_PT0_SX970_V1___.jpg"
+                />
 
             </div>
 
             <div className="home__row">
+            <Product
+                    id="3215"
+                    title="RUNMUS Gaming Headset Xbox One Headset with 7.1 Surround Sound, PS4 Headset with Noise Canceling"
+                    price={9361}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/S/aplus-media/vc/23bd0401-0601-4b2f-b281-3a7ea717ff4a._CR0,0,220,220_PT0_SX220__.jpg"
+                />
                 <Product
                     id="6457"
                     title="Book : Learning React Native: Building Native Mobile Apps..."
@@ -58,6 +72,13 @@ function Home() {
                     price={4951}
                     rating={5}
                     image="https://http2.mlstatic.com/D_NQ_NP_678176-MLA31546487400_072019-O.webp"
+                />
+                <Product
+                    id="9865"
+                    title="Sceptre 24 Curved 75Hz Gaming LED Monitor Full HD 1080P HDMI VGA Speakers, VESA Wall Mount Ready Metal Black 2019 (C248W-1920RN) "
+                    price={4951}
+                    rating={5}
+                    image="https://m.media-amazon.com/images/S/aplus-media/vc/4b482662-a3dd-411b-80d5-dbbe81b28f8d._CR0,0,1800,1800_PT0_SX300__.jpg"
                 />
             </div>
 
