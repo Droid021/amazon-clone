939b35ab65d22d0fbd98003527b0fd5faf50d4b6
Droid021 <vincivenv@gmail.com>
2020-09-19 00:23:32 +0300

    hotfix: Firebase cloud functions url

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-6dc1b/us-central1/api' // api url (cloud function url)
+    baseURL: 'http://localhost:5001/plant-288316/us-central1/api' // api url (cloud function url)
 })
 
 export default instance
