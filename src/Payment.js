0f7c7fa4a740f1e636f5db4b1e297a5df292f6df
Droid021 <vincivenv@gmail.com>
2020-09-18 11:13:56 +0300

    Payment frontend complete

--- src/Payment.js
+++ src/Payment.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Payment.css'
 import { useStateValue } from './StateProvider'
+import CheckoutProduct from './CheckoutProduct'
+import { Link } from 'react-router-dom'
 
 function Payment() {
 
@@ -8,10 +10,10 @@ function Payment() {
 
     return (
         <div className='payment'>
-
-
             <div className="payment__container">
-
+                <h1>
+                    Checkout (<Link to='/checkout'>{basket?.length} items</Link>)
+                </h1>
                 {/*payment section delivery address*/}
                 <div className="payment__section">
 
@@ -29,12 +31,33 @@ function Payment() {
 
                 {/*payment section review items*/}
                 <div className="payment__section">
-                    
+                    <div className="payment__title">
+                        <h3>Review items and delivery</h3>
+                    </div>
+
+                    <div className="payment__items">
+                        {basket.map(item => (
+                            <CheckoutProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>
                 </div>
 
                 {/*payment section payment methods*/}
                 <div className="payment__section">
+                    <div className="payment__title">
+                        <h3>Payment method</h3>
+                    </div>
 
+                    <div className="payment__details">
+                        {/* Stripe stuff*/}
+
+                    </div>
                 </div>
 
 
