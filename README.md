6b27b95ad3a0df219cf03f09bb1f0e375d9cb5e0
Droid021 <vincivenv@gmail.com>
2020-09-19 05:25:55 +0300

    Update link from localhost

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Building an amazon clone using react js and hosting on netlify and firebase
 - [x] Checkout page
 - [x] Payment processing functionality using stripe
 - [x] Firebase cloud functions (handle stripe payments)
-- [ ] Store orders for logged in users(Firestore)
+- [x] Store orders for logged in users(Firestore)
 
 ### Available Scripts
 
--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/plant-288316/us-central1/api' // api url (cloud function url)
+    baseURL: 'https://us-central1-plant-288316.cloudfunctions.net/api'
+    //'http://localhost:5001/plant-288316/us-central1/api' // api url (cloud function url)
 })
 
 export default instance
